c253055d905815edd49a046e0d20349b64bb387c
lkaveeshan <lalitharankaveeshan@gmail.com>
2022-11-08 22:34:25 +0530

    first commit

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,43 @@
+export const navbarData = [
+  {
+    routeLink: 'dashboard',
+    icon: 'fa fa-home',
+    label: 'Employees'
+  },
+  {
+    routeLink: 'products',
+    icon: 'fa fa-box-open',
+    label: 'Products'
+  },
+  {
+    routeLink: 'statistics',
+    icon: 'fa fa-chart-bar',
+    label: 'Statistics'
+  },
+  {
+    routeLink: 'coupens',
+    icon: 'fa fa-tags',
+    label: 'Coupens',
+    items: [
+      {
+        routeLink: 'coupens/list',
+        label: 'List coupens'
+      }
+    ]
+  },
+  {
+    routeLink: 'pages',
+    icon: 'fa fa-file',
+    label: 'Pages'
+  },
+  {
+    routeLink: 'media',
+    icon: 'fa fa-camera',
+    label: 'Media'
+  },
+  {
+    routeLink: 'settings',
+    icon: 'fa fa-cog',
+    label: 'Settings'
+  }
+];
